0288789a77951b8e937619ed74ebd0db8fbb91c3
kkastr <dinokastritis@gmail.com>
2015-05-27 06:56:18 -0400

    different plot module; PyPlot

--- blength.jl
+++ blength.jl
@@ -2,11 +2,12 @@ f = readdlm("output.xyz")
 
 
 k = size(f)
+s = f[1] + 2
 
 dt = zeros(0)
 dx = zeros(0)
 
-for i in 1:5:k[1]
+for i in 1:s:k[1]
 	H = [f[i+3,2],f[i+3,3],f[i+3,4]]
 	O = [f[i+2,2],f[i+2,3],f[i+2,4]]
 	md_iter = f[i+1,3]
@@ -18,7 +19,12 @@ end
 
 writedlm("tvb",[dt dx],'\t')
 
-using Gadfly
+using PyPlot
 
-p = plot(x=dt, y=dx,Coord.Cartesian(xmin=0.0,xmax=1000.0),Guide.xlabel("Time (ps)"), Guide.ylabel("Bond length (Angstrom)"),Guide.title("Bond length as a function of time in the canonical ensemble"))
-draw(SVG("tvb_pic.svg", 18cm, 9cm), p)
+
+plot(dt,dx)
+xlim([0,1000])
+xlabel(L"\mathrm{Time (fs)}")
+ylabel(L"\mathrm{Bond \, Length} (\AA)")
+title(L"\mathrm{Time \, variation\, of\, the\,OH\, bond\, length\, in\, the\, canonical\, ensemble}")
+savefig("pyplot_tvb.svg")
